7bb76e10baee9f6c4f71f4959af3b354d24b595a
smbiradar92 <smbiradar92@gmail.com>
2022-04-13 01:09:59 +0530

    Regex program validatng mobile number

--- src/com/sid/username/Implements.java
+++ src/com/sid/username/Implements.java
@@ -5,12 +5,13 @@ import java.util.regex.Pattern;
 
 public class Implements {
 
+	Scanner sc = new Scanner(System.in);
+
 	// UC 1&2: First and lastName starting with capital letter with at least 3
 	// characters.
 	public void validateName() {
 		String name = "^[A-Z][a-z]{3,}$";
 		Pattern namePattern = Pattern.compile(name);
-		Scanner sc = new Scanner(System.in);
 		String inputName = sc.next();
 
 		if (namePattern.matcher(inputName).matches()) {
@@ -26,15 +27,32 @@ public class Implements {
 	public void validateEmail() {
 		String eMail = "^[a-z0-9]{1,10}([.+-][a-z0-9]+)?@[a-z]+.[a-z]+.[a-z]{2,3}(.[a-z]{2,})?$";
 		Pattern emailPattern = Pattern.compile(eMail);
-		Scanner sc = new Scanner(System.in);
-		String inputName = sc.next();
+		String inputEmail = sc.next();
 
-		if (emailPattern.matcher(inputName).matches()) {
-			System.out.println("The entered email " + eMail
-					+ " is Valid");
+		if (emailPattern.matcher(inputEmail).matches()) {
+			System.out.println("The entered email "
+					+ inputEmail + " is Valid");
 		} else {
-			System.out
-					.println("The entered name is Invalid");
+			System.out.println(
+					"The entered email is Invalid");
+		}
+
+	}
+
+	public void validateMobileNum() {
+//		String mobileNum = "[+]?[0-9]{2}[ ][0-9]{6,11}$";
+		String mobileNum = "^([+]?[1-9]{2})[\\s][0-9]{1,10}$";
+	
+		Pattern mobileNumPattern = Pattern
+				.compile(mobileNum);
+		String inputMob = sc.next();
+
+		if (mobileNumPattern.matcher(inputMob).matches()) {
+			System.out.println("The entered Mobile number "
+					+ inputMob + " is Valid");
+		} else {
+			System.out.println(
+					"The entered Mobile number is Invalid");
 		}
 
 	}
--- src/com/sid/username/UserRegistration.java
+++ src/com/sid/username/UserRegistration.java
@@ -5,16 +5,20 @@ public class UserRegistration {
 	public static void main(String[] args) {
 
 		Implements impl = new Implements();
-		System.out.println("Enter the FirstName");
-		impl.validateName();
-		System.out.println(
+//		System.out.println("Enter the FirstName");
+//		impl.validateName();
+//		System.out.println(
+//				"<------------------------------------------------>");
+//		System.out.println("Enter the LastName");
+//		impl.validateName();
+//		System.out.println(
+//				"<------------------------------------------------>");
+//		System.out.println("Enter the email id");
+//		impl.validateEmail();
+	System.out.println(
 				"<------------------------------------------------>");
-		System.out.println("Enter the LastName");
-		impl.validateName();
-		System.out.println(
-				"<------------------------------------------------>");
-		System.out.println("Enter the email id");
-		impl.validateEmail();
+		System.out.println("Enter the mobileNumber");
+		impl.validateMobileNum();
 		System.out.println(
 				"<------------------------------------------------>");
 
