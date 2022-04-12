0f5a5235740c822eeb9a1e084820b2c658b74715
smbiradar92 <smbiradar92@gmail.com>
2022-04-12 22:45:37 +0530

    Regex program for entering FirstName and LastName

--- src/com/sid/username/UserRegistration.java
+++ src/com/sid/username/UserRegistration.java
@@ -7,18 +7,32 @@ public class UserRegistration {
 
 	public static void main(String[] args) {
 		String str1 = "^[A-Z][a-z]{3,}$";
-		
-		Pattern namePattern = Pattern.compile(str1);
+		String str2 = "^[A-Z][a-z]{3,}$";
+
+		Pattern fnamePattern = Pattern.compile(str1);
 		System.out.println("Enter the FirstName");
-		
+
 		Scanner sc = new Scanner(System.in);
 		String fName = sc.next();
-		
-		if(namePattern.matcher(fName).matches()) {
-			System.out.println("Valid");
-		}else {
-			System.out.println("Invalid");
+//		
+//		if(fnamePattern.matcher(fName).matches()) {
+//			System.out.println("Valid");
+//		}else {
+//			System.out.println("Invalid");
+//		}
+
+		Pattern lnamePattern = Pattern.compile(str1);
+		System.out.println("Enter the LastName");
+		String lName = sc.next();
+
+		if (lnamePattern.matcher(lName).matches()
+				&& fnamePattern.matcher(fName).matches()) {
+			System.out.println(
+					"The entered firstName and LastName is Valid");
+		} else {
+			System.out.println(
+					"The entered firstName and LastName is Invalid");
 		}
-		
+
 	}
 }
